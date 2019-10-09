8b71d4287cfe4431b428064fb5b38b6b6f010db6
arthurproducer <arthurproducer@gmail.com>
2019-10-08 22:34:12 -0300

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("oi");
-//console.log(process.env.NODE_ENV);
-console.log('MEU AMBIENTE: ', process.env.NODE_ENV);
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
+
+
+//criação de toras
+app.get('/users',(request,response,next)=>{
+    //console.log(request.query.id);
+
+    console.log("Entrei na rota users");
+    //response.sendStatus(200);
+    response.json({sucess: true});
+})
+
+//console.log("oi");
+console.log('AMBIENTE: ', process.env.NODE_ENV);
+
