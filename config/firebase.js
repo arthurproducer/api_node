15d375c32b67b177b75ef3fd9297d892483e5ff3
arthurproducer <arthurproducer@gmail.com>
2019-10-14 17:09:13 -0300

    atualização aula de quinta

---
+++ config/firebase.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apiKey: "AIzaSyB3TpQbcdY2jcrwVtvWmcGVlxgg0Vry7f0",
+    authDomain: "node-api-a9c5a.firebaseapp.com",
+    databaseURL: "https://node-api-a9c5a.firebaseio.com",
+    projectId: "node-api-a9c5a",
+    storageBucket: "node-api-a9c5a.appspot.com",
+    messagingSenderId: "812730198580",
+    appId: "1:812730198580:web:5c7d6cf6d989f865b73861",
+    measurementId: "G-SCM1EWWQBY"
+  };
+ 
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 
+//FIREBASE
+const firebase = require('firebase');
+const firebaseConfig = require('./config/firebase');
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+//console.log(firebaseApp);
+
+const db = firebaseApp.firestore();
+
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -10,13 +21,60 @@ app.listen(port, () => {
 
 //criação de toras
 app.get('/users',(request,response,next)=>{
-    //console.log(request.query.id);
+    
+    /**
+    * BUSCAR TODOS OS DADOS DA COLEÇÃO 'users'
+    */
+    
+    db.collection('users').get()
+        .then(snapshot => {
+            
+            const result = []
+            
+            snapshot.forEach(doc => {
+                result.push(doc.data());
+            });
 
+
+            /*
+            .then(users => response.json(
+                users.docs.map(user =>({
+                    ...user.data(),
+                    id: user.id,
+                }))
+            ))
+            */ 
+                        response.json(result);
+            
+        }).catch(err =>{
+            console.log('Error getting document', err);
+        });
+    
+    //console.log(request.query.id);
+    
     console.log("Entrei na rota users");
     //response.sendStatus(200);
-    response.json({sucess: true});
-})
+    //response.json({sucess: true});
+});
+
+app.get('/users/:id',(request,response,next) => {
+    const id = request.params.id;
+    
+    //response.json({sucess: 'OII estou na nova'});
+    db.collection('users').doc(id).get()
+    .then(user => {    
+        if(!user.exists){
+            return response
+            .status(404)
+            .send({message:'No Content'})   
+        }
+        //console.log('user',user.data());
+        response.json(users.data());
+    })
+    .catch(err =>{
+        console.log('Error getting document', err);
+    });
+});
 
 //console.log("oi");
 console.log('AMBIENTE: ', process.env.NODE_ENV);
-
